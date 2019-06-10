e3ecd2db237e68171bffe140efeee4fbc9d33d6a
majorkira <majorkira1@hotmail.com>
2019-06-10 14:34:28 -0400

    added eyes

---
+++ eyez.js
@@ -0,0 +1,17 @@
+let balls = document.querySelectorAll(".ball");
+
+balls.forEach(ball => {
+  document.addEventListener("mousemove", moveEyez, false);
+});
+
+function moveEyez() {
+  balls.forEach(ball => {
+    let x = (event.clientX * 100) / window.innerWidth + "%";
+    let y = (event.clientY * 100) / window.innerHeight + "%";
+    console.log(x, y);
+    console.log(window.innerWidth, window.innerHeight);
+    ball.style.left = x;
+    ball.style.top = y;
+    ball.style.transform = "translate(-" + x + ",-" + y + ")";
+  });
+}
